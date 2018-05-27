20c5ce942d31039ee6bd29f9f47f5636b49b8489
Steven Love <lovestevend@gmail.com>
2018-05-27 11:54:15 -0400

    wip

--- src/ConceptList/Concept.js
+++ src/ConceptList/Concept.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 
 class Concept extends Component {
     // name="hi";
+
+    markers = [];
+
+
+    handleClick = event => {
+        console.log("event",event);
+        event.currentTarget.style.backgroundColor = '#cc0000';
+    }
+
     render(args) {
         let name = this.props.name;
         let imgSrc = "img/"+name+".png";
         let altText = name;
         console.log("this",this);
         return (
-            <div>
-                <img src={imgSrc} alt={altText}></img>
-            </div>
+            <span className="concept">
+                <img onClick={this.handleClick} src={imgSrc} alt={altText}></img>
+                <div class="marker">hi</div>
+            </span>
         );
     }
 }
